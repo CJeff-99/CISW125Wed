59718311eb73f33ad9dde832d636322510a3e8d5
CJeff-99 <soldierdragon99@gmail.com>
2026-08-26 14:23:55 -0600

    Added week 2 lab file

---
+++ Homework.py
@@ -0,0 +1,65 @@
+# Name variables in realtion to what their value is 
+game= "Minecraft"
+# Print is what shows program output
+print(game) 
+
+character= "Ricky"
+# if you want number do not put interger in quotes ""
+score=5
+
+# Variable names should describe what they contain
+
+#bad examples of Variable names
+x=50
+a="Minecraft"
+thing= "Ricky"
+
+#Variable Rules 101:
+#Variables Names cannot contain spaces
+amount_of_oranges=10
+#Cannot begin with a number
+
+#Should usually start with a lowercase
+#Use underscores to seperate words
+
+
+x=5
+y=3
+
+print("I like the game", game, "Because it is fun")
+
+print(f"My fav game is {game}")
+
+print(f"Hamburgers are the best in {game}")
+
+favfood="hamburger"
+print(f"My fave food is {favfood}")
+
+
+total=5+3
+print(total)
+
+
+
+addition=5+5 #output would be 10
+subtraction=5-5 #output would be 0
+multiplication=5*5 #output would be 25      
+division=10/5 #output would be 2
+
+# + is for addition 
+# - is for subtraction 
+# * is for multiplication 
+# / is for division 
+# % is for showing the remainder % is called a modulus
+# ** is for exponents 
+# // is for floor division
+
+
+number=10//3
+print(number)
+
+fname="Ricky"
+lname="Bobby"
+#If we want to add strings together is it called string concatenation
+full_name=fname+lname 
+print(full_name)
---
+++ Variables and Expressions Lab.py
@@ -0,0 +1,57 @@
+# CISW 125
+# Intro to programming
+
+# Follow the Documentation Policy as it's good practice and will get you used to what you should do for
+# your projects and other labs.
+
+# If you're stuck, ask questions. There are no dumb questions.
+# ------------------------------------------------------------------------------------------------------
+# We're going to play around with variables and expressions today
+# The goal is to just test out things and put them to use and possibly
+# save the ideas/work for projects down the line.
+
+# Again, the goal is to "play" around and explore. There's no right or wrong to this.
+# Just think about input vs output and what we can do with them.
+# ------------------------------------------------------------------------------------------------------
+
+
+# Create some variables, give them a theme. Example: items on a grocery list, games/books/movies you enjoy, etc.
+favVideoGame="Halo3"
+leastFavGame="Fable3"
+newgame="DuskBloods"
+# Then print your variables.
+print(favVideoGame)
+print(leastFavGame)
+# Now try to reassign a value. This is essentially "overwriting" your variables data with new data. Python works from top to bottom.
+favVideoGame="Gears_of_War"
+# After you've done this, try to print your variables in string using f-strings.
+print(f"my fav game is {favVideoGame}")
+
+# Next, try to create some expressions that involve addition, subtraction, multiplication, and division
+additon=10+5
+division=300/6
+# Store the results of your expressions in a variable and then print the outcome
+print(additon)
+print(division)
+
+# See if you can find other ways to "do maths" (hint: operators are useful and efficient.)
+# https://www.w3schools.com/python/python_operators.asp
+
+
+# Now, I'd like you to make two variables that contain your first and last name
+# After you've made the variables, find a way to join the two strings to print your full name. This is string concatenation.
+# Think of it as "adding" your variables together.
+fname= "Connor"
+lname="Jeff"
+full_name=fname+lname
+print(full_name)
+
+
+# While we did some math earlier, I'd like you to try doing math with variables this time. (If you already did this, you can skip this. Good job.)
+multiplication=30*30
+print(multiplication)
+
+# Lastly, do something of your own choice. Anything that involves variables and expressions is allowed here.
+# If you're stumped on ideas, just try and make an expression that converts Celsius to Fahrenheit or vice versa.
+
+# Upload this to Canvas under the Variable and Expressions Lab assignment.
