b6e4cb31143d0088dace6ae321b4c660b9c545df
Connor Jeff <soldierdragon99@gmail.com>
2026-09-02 22:17:50 -0600

    Asked favorite music

---
+++ Projects/project0-2.py
@@ -0,0 +1,15 @@
+food= "Waffles"
+
+user_text=input(f"do you like {food}?: ")
+
+user_response=input("I like waffles too ")
+
+user_text=input(f"wanted to ask again do you like {food}?: ")
+
+
+user_text=input(f"do you like chocolate chips with them? {food}?: ")
+
+
+
+
+
